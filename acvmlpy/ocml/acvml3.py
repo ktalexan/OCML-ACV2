a815e46428f1dc3a5cba819fd69abc0d687a338d
Kostas Alexandridis <ktalexan@outlook.com>
2022-11-08 09:48:44 -0800

    updated settings

--- acvmlpy/ocml/acvml3.py
+++ acvmlpy/ocml/acvml3.py
@@ -28,8 +28,7 @@ import pyproj
 import requests
 import xlrd
 import xlsxwriter as xlw
-from azure.storage.blob import (BlobClient, BlobServiceClient, ContainerClient,
-                                __version__)
+from azure.storage.blob import (BlobClient, BlobServiceClient, ContainerClient, __version__)
 from GPSPhoto import gpsphoto
 from IPython.display import display
 from pandas.io.json import json_normalize
